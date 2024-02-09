de6525b1d35b2078a312e514ecd03880c8063487
Alphi-Udayakumar.S <Alphi-Udayakumar.S@ALPHI-UDAYAKUMA>
2024-02-09 09:06:52 +0530

    Added new column

--- SQL-DB/dbo/Tables/tblFamily.sql
+++ SQL-DB/dbo/Tables/tblFamily.sql
@@ -2,6 +2,7 @@
     [FamilyID]       INT            IDENTITY (1, 1) NOT NULL,
     [FamilyName]     NVARCHAR (255) NULL,
     [ParentFamilyId] INT            NULL,
+    [ParentGrand] NCHAR(10) NULL, 
     PRIMARY KEY CLUSTERED ([FamilyID] ASC)
 );
 
